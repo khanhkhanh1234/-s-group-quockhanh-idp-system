045208bdca3ea0576dbd06f2be0e185fcf4c0287
Quoc Khanh <nguyenkimt322@gmail.com>
2024-02-16 17:25:05 +0700

    feature: login done

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,13 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginCredentials } from './interface';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-
   @Post('login')
-  async login(
-    @Body() credentials: { username: string; password: string },
-  ): Promise<LoginCredentials> {
-    const { username, password } = credentials;
-    return this.authService.login(username, password);
+  login(@Body() loginDto: LoginDto): Promise<LoginCredentials> {
+    return this.authService.login(loginDto);
   }
 }

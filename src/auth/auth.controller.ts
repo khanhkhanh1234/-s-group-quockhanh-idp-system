fda882bc7b7dee594fa705d9f47c3462650c399e
Quoc Khanh <nguyenkimt322@gmail.com>
2024-02-25 02:46:53 +0700

    feat: guard  api with jwt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, UseGuards, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { LoginCredentials } from './interface';
+import { LocalAuthGuard } from './guard/local-auth.guard';
+import { LoginCredentials } from './interface/interface';
 import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+  @UseGuards(LocalAuthGuard)
   @Post('login')
   login(@Body() loginDto: LoginDto): Promise<LoginCredentials> {
     return this.authService.login(loginDto);
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,31 +1,27 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import Role from 'src/roles/entities/role.entity';
 import User from 'src/users/entities/user.entity';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { JwtStrategy } from './jwt.stragery';
+import { UsersModule } from 'src/users/users.module';
+import { LocalStrategy } from './strategies/local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
+    UsersModule,
     TypeOrmModule.forFeature([User, Role]),
-    PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => {
-        return {
-          secret: configService.get('JWT_SECRET'),
-          signOptions: { expiresIn: '60s' },
-        };
-      },
+    PassportModule,
+    JwtModule.register({
+      secret: 'process.env',
+      signOptions: { expiresIn: '1d' },
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy],
-  exports: [JwtStrategy, PassportModule],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}

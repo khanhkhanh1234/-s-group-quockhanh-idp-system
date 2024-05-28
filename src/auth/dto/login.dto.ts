2f0552ab89f33569d8bb92355c9f1d93eb6dfc69
Quoc Khanh <nguyenkimt322@gmail.com>
2024-05-28 21:19:56 +0700

    demo git flow

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,11 +1,6 @@
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginDto {
-  @IsNotEmpty()
-  @IsString()
   username: string;
-
-  @IsNotEmpty()
-  @IsString()
   password: string;
 }
